07411fc57416cbbe565e36525da1526b27d35193
yves songolo <yves.songolo@gmail.com>
2017-09-10 11:47:28 -0700

    OOP_example: problem14,15

--- OOP_example.playground/Contents.swift
+++ OOP_example.playground/Contents.swift
@@ -23,7 +23,7 @@ var str = "Hello, playground"
 
  
  */
-class car {
+class Automobile {
     var numberOfWheels: Int?
     var make: String?
     var model: String?
@@ -42,7 +42,7 @@ class car {
     
     }
     init(){
-        self.numberOfWheels = 0
+        self.numberOfWheels = 4
         self.color = ""
         self.model = ""
         self.topSpeed = 0
@@ -50,22 +50,63 @@ class car {
     }
     init(color: String!){
         self.color = color
-        self.numberOfWheels = 0
+        self.numberOfWheels = 4
         self.model = ""
         self.topSpeed = 0
         self.make = ""
     }
-    
+}
     /*
      
      Object Oriented is a concept to reprsent an object and his characteristic programatically. therefore, inheritance is a concept to pass  a characteristic from a one class (base class) to another one (child class) for example Trump daddy pass his fortune to tromp Junior, or a mammy bird pass his technic of flying to baby bird
      
      */
     
+    /*Problem 15:  Superclasses
     
+    Modify your code in OOP_examples.py from Problem 13.  Create a superclass called Automobile, and 3 subclasses that inherit from it:
     
+    Car
+    SemiTruck
+    Motorcycle
     
-    
-    
+    These classes should be semantically accurate to their real-world counterparts. For instance, objects of class Motorcycle should have two wheels instead of four.  For all three classes, the color attribute should be settable during instantiation.
+
+    */
+
+class car: Automobile{
+    override init(color: String!){
+        super.init()
+        self.numberOfWheels = 4
+        self.color = color
+        self.model = ""
+        self.topSpeed = 0
+        self.make = ""
+    }
+}
+
+class Motorcycle: Automobile{
+    override init(color: String!){
+        super.init()
+        self.numberOfWheels = 2
+        self.color = color
+        self.model = ""
+        self.topSpeed = 0
+        self.make = ""
+    }
     
 }
+class semiTruck: Automobile{
+    override init(color: String!){
+        super.init()
+        self.numberOfWheels = 4
+        self.color = color
+        self.model = ""
+        self.topSpeed = 0
+        self.make = ""
+    }
+
+}
+
+
+
